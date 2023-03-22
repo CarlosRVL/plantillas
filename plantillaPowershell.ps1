817017d49896a96e85dbbe3c74e0e67371154d7f
CarlosRVL <37228433+CarlosRVL@users.noreply.github.com>
2023-03-22 23:55:14 +0100

    Create plantillaPowershell.ps1

---
+++ plantillaPowershell.ps1
@@ -0,0 +1,34 @@
+<#
+.SYNOPSIS
+Descripción del script
+
+.DESCRIPTION
+Descripción detallada del script
+
+.PARAMETER parametro1
+Descripción del parámetro 1
+
+.PARAMETER parametro2
+Descripción del parámetro 2
+
+.EXAMPLE
+Ejemplo de uso del script
+
+.EXAMPLE
+Ejemplo alternativo de uso del script
+
+.INPUTS
+Descripción de los objetos de entrada
+
+.OUTPUTS
+Descripción de los objetos de salida
+#>
+
+Param(
+    [Parameter(Mandatory=$true)]
+    [string]$Parametro1,
+    [Parameter(Mandatory=$false)]
+    [int]$Parametro2 = 0
+)
+
+# Código del script
