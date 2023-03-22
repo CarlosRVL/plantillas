499564e6fc7275ce3f9047e5fd393f8fd2b8b3a5
CarlosRVL <37228433+CarlosRVL@users.noreply.github.com>
2023-03-22 23:53:30 +0100

    Create plantillaPython.py

---
+++ plantillaPython.py
@@ -0,0 +1,44 @@
+
+def nombre_funcion(argumento1, argumento2):
+    """
+    Documentación de la función
+
+    :param argumento1: Descripción del argumento 1
+    :type argumento1: Tipo del argumento 1
+    :param argumento2: Descripción del argumento 2
+    :type argumento2: Tipo del argumento 2
+    :return: Descripción del valor devuelto
+    :rtype: Tipo del valor devuelto
+    """
+    # Código de la función
+    return resultado
+
+class NombreClase:
+    """
+    Documentación de la clase
+    """
+
+    def __init__(self, argumento1, argumento2):
+        """
+        Documentación del constructor
+
+        :param argumento1: Descripción del argumento 1
+        :type argumento1: Tipo del argumento 1
+        :param argumento2: Descripción del argumento 2
+        :type argumento2: Tipo del argumento 2
+        """
+        # Inicialización de la clase
+
+    def nombre_metodo(self, argumento1, argumento2):
+        """
+        Documentación del método
+
+        :param argumento1: Descripción del argumento 1
+        :type argumento1: Tipo del argumento 1
+        :param argumento2: Descripción del argumento 2
+        :type argumento2: Tipo del argumento 2
+        :return: Descripción del valor devuelto
+        :rtype: Tipo del valor devuelto
+        """
+        # Código del método
+        return resultado
